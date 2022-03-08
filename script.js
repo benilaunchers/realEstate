e083e1bac15379b0d0d8b65d6857703acafb0dad
mohammad <benyaminbusiness81@gmail.com>
2022-03-08 13:10:47 +0330

    example section created and somebug fixes

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const images = document.querySelectorAll("#slider img");
 let current = 0;
 nextBtn.addEventListener("click", () => {
         images[current].classList.remove("img-active");
-        current++;
+        // if(current >= (images.length - 1)) {
+        //     current = -1;
+        // }
+        // current++;
+        current = (current + 1) % images.length; //you can write this or above Code
         images[current].classList.add("img-active");
         console.log(current);
-        if(current >= (images.length)) {
-            current = 0;
-        }
+});
+prevBtn.addEventListener("click", () => {
+    console.log(current);
+    images[current].classList.remove("img-active");
+    if(current <= 0){
+        current = images.length;
+    }
+    current--;
+    images[current].classList.add("img-active");
 });
